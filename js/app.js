bc1db6166d5a5592b301870a11774abd2aa251e8
Jola <recka.jolanta@gmail.com>
2019-12-03 14:09:59 +0100

    wallet - get, check and change funcions added

--- js/app.js
+++ js/app.js
@@ -2,4 +2,4 @@ import Wallet from "./components/wallet/Wallet";
 
 const wallet = new Wallet(200);
 
-document.body.textContent = wallet.name;
+document.body.textContent = wallet.changeWallet(NaN, "+");
--- js/components/wallet/Wallet.js
+++ js/components/wallet/Wallet.js
@@ -1,7 +1,28 @@
 class Wallet {
     constructor(money) {
-        this.name = money;
         let _money = money;
+
+        // get current wallet content
+        this.getWalletValue = () => _money;
+
+        // checking if the user has sufficient funds
+        this.checkCanPlay = value => {
+           return _money >= value;
+        }
+        this.changeWallet = (value, type = "+") => {
+            if(typeof value === "number" && !isNaN(value)) {
+                if(type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("invalid operation mode");
+                }
+            } else {
+                console.log(typeof value);
+                throw new Error("invalid number")
+            }
+        }
      }
 }
 
